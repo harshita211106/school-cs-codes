c33241f67b1700ccb7635ee85d2eae08d2a33a67
harshita211106 <114283416+harshita211106@users.noreply.github.com>
2022-09-29 11:49:27 +0530

    Add files via upload

---
+++ wap to find multiples of a number out of 5 num.py
@@ -0,0 +1,14 @@
+## program to find the multiples of a number (the divisor)out of the given five number
+
+print("Enter five numbers below")
+num1=float(input("First Number: "))
+num2=float(input("Second Number: "))
+num3=float(input("Third Number: "))
+num4=float(input("Fourth Number: "))
+num5=float(input("Fifth Number: "))
+divisor=float(input("Enter divisor number: "))
+
+
+print("Multiples of",divisor,"are")
+remainder=num1%divisor
+if remainder == 
