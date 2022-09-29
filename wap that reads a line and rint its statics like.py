51652ae88f5a9190a554e1a4d50f97d3b9df4781
harshita211106 <114283416+harshita211106@users.noreply.github.com>
2022-09-29 11:48:46 +0530

    Add files via upload

---
+++ wap that reads a line and rint its statics like.py
@@ -0,0 +1,8 @@
+## program thet reads a line and prints its statistics like:
+
+line = input("Enter a line :")
+lowercase=uppercase=0
+digitcount=alphacount=symcount=0
+
+for a in line :
+    if a.is lpwer 
