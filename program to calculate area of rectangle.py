3831bbcdf21d572ecfdef3be12152b3dc8526e43
harshita211106 <114283416+harshita211106@users.noreply.github.com>
2022-09-29 11:47:17 +0530

    Add files via upload

---
+++ program to calculate area of rectangle.py
@@ -0,0 +1,8 @@
+
+#wap to enter length and breath and calculate area of rectangle
+
+length=float(input("enter length"))
+width=float(input("enter width"))
+area=length*width
+print("area of rectangle=",area)
+
