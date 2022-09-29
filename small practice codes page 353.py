0f59655b19bc70c1d535b5998e7d0afab95d5daa
harshita211106 <114283416+harshita211106@users.noreply.github.com>
2022-09-29 11:48:02 +0530

    Add files via upload

---
+++ small practice codes page 353.py
@@ -0,0 +1,17 @@
+#code to find length of string
+def length():
+    width="harshita bhingare"
+    print(len(width))
+length()
+
+##code to capitalize any string
+def capital():
+    print("helloworld".capitalize())
+
+capital()
+
+##code to count(occurence) of substrings
+
+def occurence():
+    sub="are"
+    print("how are you?".count(sub[,st[,end]]))
